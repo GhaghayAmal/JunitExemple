1ed8cc4310da347df1125b2df7522c98df6088fb
AMAL <AMAL@192.168.1.22>
2016-11-01 14:38:30 +0100

    JunitTest

---
+++ src/main/java/executionProcedure/TestRunnerExecutionPrc.java
@@ -0,0 +1,16 @@
+package executionProcedure;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunnerExecutionPrc {
+	public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(ExecutionProcedureJunit.class);
+
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+			
+	      System.out.println(result.wasSuccessful());
+	   }
+}
